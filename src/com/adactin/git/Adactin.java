72cb4c65a9874259ff54147c75e9be757e8ba474
pavithra24297 <rajalakshmi2497@gmail.com>
2020-06-23 19:51:13 +0530

    First Commit

---
+++ src/com/adactin/git/Adactin.java
@@ -0,0 +1,25 @@
+package com.adactin.git;
+
+public class Adactin {
+	
+	public static void star() {
+		
+		for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j <= i; j++) {
+
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		star();
+
+	}
+}
+
+
+
